434cdd86ac20b8f302c5a14134e170ff91e4de6e
jryan <joseph.ryan@laterooms.com>
2016-06-17 08:36:36 +0100

    wiring up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const createRancher = require('./rancher');
+const createMasterScheduler = require('./masterSchedule');
+const getConfig = require('./config');
 
+const run = () => {
+    const config = getConfig();
+    const rancher = createRancher(config);
+    const masterSchedule = createMasterScheduler(rancher, config);
+    masterSchedule.start();
+}
+run();
