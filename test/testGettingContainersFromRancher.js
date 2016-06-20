c44333b283495b2a9372bd4a57f0173d4cb3b683
jryan <joseph.ryan@laterooms.com>
2016-06-20 09:54:50 +0100

    failing test for scheduling jobs based on response

--- test/testGettingContainersFromRancher.js
+++ test/testGettingContainersFromRancher.js
@@ -15,20 +15,22 @@ describe('Scheduling Rancher Checks', () => {
         });
     });
     describe('Given a rancher check will run immediately', () => {
+        let scheduleJobRan = () => {};
+        const config = {
+            host: 'localhost',
+            port: 1234,
+            labels: {
+                path:'/a/rancher/resource'
+            }
+        }
         const cronScheduler = {
             scheduleJob: (spec, job) => {
                 job();
+                scheduleJobRan();
             }
         }
         describe('When the application starts', () => {
             it('should ask rancher for a label', () => {
-                const config = {
-                    host: 'localhost',
-                    port: 1234,
-                    labels: {
-                        path:'/a/rancher/resource'
-                    }
-                }
                 const rancherInterface = createRancherInterface(config);
                 const rancherCheckScheduler = createRancherCheckScheduler(cronScheduler, rancherInterface, config);
                 rancherCheckScheduler.start();
@@ -37,6 +39,29 @@ describe('Scheduling Rancher Checks', () => {
                 return new Promise(resolve => setTimeout(() => resolve(urlRequested), 20))
                     .should.eventually.equal('/a/rancher/resource')
             });
-        })
-    })
+        });
+        describe('And there are some containers with a cron spec label', () => {
+            const labelResponse = ['container1', 'container2', 'container3']
+            fakeRancher.start((req, res) => {
+                res.write(JSON.stringify(containersWithLabel));
+                res.end();
+            });
+            describe('When I ask rancher for the containers to schedule', () => {
+                it('there should be a job scheduled for the label check and each container', () => {
+                    let jobsScheduled = 0;
+                    scheduleJobRan = () => {
+                        jobsScheduled++;
+                    }
+                    const rancherInterface = createRancherInterface(config);
+                    const rancherCheckScheduler = createRancherCheckScheduler(cronScheduler, rancherInterface, config);
+                    rancherCheckScheduler.start();
+                    return new Promise(resolve => setTimeout(() => resolve(jobsScheduled), 50))
+                        .should.eventually.equal(4);
+                });
+            });
+        });
+    });
+    afterEach(() => {
+        fakeRancher.stop();
+    });
 });
