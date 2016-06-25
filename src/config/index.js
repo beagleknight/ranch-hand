65b11f678f43797d7d0e4962cf506de8a09a0d25
jryan <joseph.ryan@laterooms.com>
2016-06-25 12:11:04 +0100

    hacky way to replace config if config.json doesn't exist

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,10 @@
-let config = require('./config.json');
+let config;
+try {
+    config = require('./config.json');
+}
+catch (err) {
+    console.error("couldn't load config.json, using defaults", err.stack)
+}
 if(!config) {
     config = require('./defaults.json')
 }
