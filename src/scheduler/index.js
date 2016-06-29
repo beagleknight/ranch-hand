01d3e0b75d57845b4da838fc5f0dc1907f1bea65
jryan <joseph.ryan@laterooms.com>
2016-06-29 08:13:02 +0100

    improving start request logging

--- src/scheduler/index.js
+++ src/scheduler/index.js
@@ -3,14 +3,14 @@ const logger = require('../logging')
 
 module.exports = function(rancher) {
     const scheduledChecks = {};
-    const makeRancherCall = path => {
-        logger.logInfo('making rancher request', {path: path})
+    const makeRancherCall = (path, spec) => {
+        logger.logInfo('making rancher request on spec', {path: path, cron_spec: spec})
         rancher.makeRequest(path);
     }
 
     const scheduleAndSave = (path, spec) => {
         scheduledChecks[path] = {};
-        scheduledChecks[path].job = nodeSchedule.scheduleJob(path, spec, () => makeRancherCall(path));
+        scheduledChecks[path].job = nodeSchedule.scheduleJob(path, spec, () => makeRancherCall(path, spec));
         scheduledChecks[path].spec = spec;
         logger.logInfo('scheduled start', {cron_spec: spec, rancher_path:path});
     }
